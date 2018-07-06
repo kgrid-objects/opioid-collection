f1289f27c6df646dd288bf884786e4d69d5e4fbc
Greg Farris <greg.t.farris@gmail.com>
2018-07-06 10:03:04 -0400

    Added script to zip up KOs for release

---
+++ scripts/release.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env sh
+
+rm -rf dist
+mkdir -p dist/shelf && cp -rf 99999-10101 dist/shelf/99999-10101 && cp -rf 99999-10102 dist/shelf/99999-10102 && cp -rf 99999-10103 dist/shelf/99999-10103
+cd dist && zip -r -X opioid-shelf.zip shelf -x \"*.DS_Store\"
