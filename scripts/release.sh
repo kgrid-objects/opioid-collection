b3c1a11272a5ea605d63082df8d5f5c3a41ba1f5
Greg Farris <greg.t.farris@gmail.com>
2018-07-10 11:12:18 -0400

    script to create zip files of KOs individual and as shelf

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env sh
 
 rm -rf dist
-mkdir -p dist/shelf && cp -rf 99999-10101 dist/shelf/99999-10101 && cp -rf 99999-10102 dist/shelf/99999-10102 && cp -rf 99999-10103 dist/shelf/99999-10103
-cd dist && zip -r -X opioid-shelf.zip shelf -x \"*.DS_Store\"
+mkdir -p dist/shelf
+cp -rf 99999-10101 dist/shelf/99999-10101
+cp -rf 99999-10102 dist/shelf/99999-10102
+cp -rf 99999-10103 dist/shelf/99999-10103
+cd dist/shelf
+zip -r -X ../99999-10101.zip 99999-10101 -x \"*.DS_Store\"
+zip -r -X ../99999-10102.zip 99999-10102 -x \"*.DS_Store\"
+zip -r -X ../99999-10103.zip 99999-10103 -x \"*.DS_Store\"
+cd ..
+zip -r -X opioid-shelf.zip shelf -x \"*.DS_Store\" -x \"*.zip\"
+
