17693b5d4d740e81ba3523c83806e679957eea9c
Greg Farris <greg.t.farris@gmail.com>
2018-09-21 09:38:49 -0400

    Added load testing scripts

---
+++ tests/k6/99999-10101.js
@@ -0,0 +1,25 @@
+
+import http from "k6/http";
+
+import { check } from "k6";
+const params = {headers: {'Content-Type': 'application/json'}};
+var hostname = __ENV.HOSTNAME === undefined ? 'localhost:8080' : __ENV.HOSTNAME;
+var testObjects = [
+  {input: {"rxcuis":"200240,856917,994226,197446,801958"}, opioid: true, description:"opioid should be found"},
+  {input: {"rxcuis":"200240,856917,197446"}, opioid: false, description:"opioid should NOT be found"},
+
+]
+
+export default function() {
+  var url = `http://${hostname}/99999/10101/v0.0.1/opioidDetector`;
+
+  var randomtestObject = testObjects[Math.floor(Math.random() * testObjects.length)];
+
+  var result = http.post(url, JSON.stringify(randomtestObject.input), params);
+  check(result, {
+    'is status 200': (r) => r.status === 200,
+      "Opioid": (r) => JSON.parse(r.body).result.summary.opioid == randomtestObject.opioid
+
+});
+
+};
---
+++ tests/k6/99999-10103.js
@@ -0,0 +1,25 @@
+
+import http from "k6/http";
+
+import { check } from "k6";
+const params = {headers: {'Content-Type': 'application/json'}};
+var hostname = __ENV.HOSTNAME === undefined ? 'localhost:8080' : __ENV.HOSTNAME;
+var testObjects = [
+  {input: {"rxcuis":"106500,200240,856917,994226,197446,801958"}, bzd: true, description:"bzd should be found"},
+  {input: {"rxcuis":"56917,994226"}, bzd: false, description:"bzd should NOT be found"},
+
+]
+
+export default function() {
+  var url = `http://${hostname}/99999/10103/v0.0.1/tripleThreatDetector`;
+
+  var randomtestObject = testObjects[Math.floor(Math.random() * testObjects.length)];
+
+  var result = http.post(url, JSON.stringify(randomtestObject.input), params);
+  check(result, {
+    'is status 200': (r) => r.status === 200,
+      "Opioid": (r) => JSON.parse(r.body).result.summary.bzd == randomtestObject.bzd
+
+});
+
+};
