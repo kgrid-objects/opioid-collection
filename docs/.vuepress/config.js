f001964fcd4c71298d12caf417d47fdeaaa4776d
George Meng <gqmeng.meng@gmail.com>
2018-10-10 12:30:41 -0400

    Enabled circleci; included kgrid.org link; added logo

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,10 +2,11 @@ module.exports = {
   base: '/opioid-collection/',
   title: 'Opioid Collection',
   themeConfig: {
-    // logo: './images/kgrid-logo.png',
+    logo: './images/kgrid-logo.png',
     repo: 'kgrid-objects/opioid-collection',
     lastUpdated: 'Last Updated',
     nav: [
+      { text: 'KGrid.org', link: 'https://kgrid.org' },
       { text: 'Guide', link: '/' },
       // { text: 'Develop', link: '/develop/' },
       // { text: 'CPIC Collection', link: 'https://kgrid-objects.github.io/cpic-collection'},
